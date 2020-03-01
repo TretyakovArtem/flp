cac8d04bf4aa8f7ec078b3111578a1262890c1bd
artem.tretyakov.91@inbox.ru <artem.tretyakov.91@inbox.ru>
2020-03-01 17:20:18 +0300

    add lost file

---
+++ hello.lisp
@@ -0,0 +1,23 @@
+(CAR(CDR'((HEIGHT 72)(WEIGHT 175)(HAIR BLOND))))
+
+lesson 3
+
+(1 2 goal 3 4)
+
+(CAR(CDR(CDR(1 2 goal 3 4))))
+
+(CADDR'(1 2 goal 3 4))
+
+
+CADDDDDDR((1 (2 (3 4 goal))))
+
+(CDDAR(CDADAR'((1 (2 (3 4 goal))))))
+
+(X Y Z)
+
+(A B C)
+
+
+(CONS'(CAR(A B C) CDR(X Y Z)))
+
+ (CONS (CAR'(A B C)) (CDR'(X Y Z)))
